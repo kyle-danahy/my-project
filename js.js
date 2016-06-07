5789e5de0f72f291f84548f2ffda7a7276df4252
magicbunny25 <brian25@cinci.rr.com>
2016-06-07 19:52:05 -0400

    JS
    JS

---
+++ js.js
@@ -0,0 +1,16 @@
+var summonerName = "";
+var key = "";
+key = $("4d5b95a2-e742-43c9-8d5e-3bec80f09bfe")
+
+function submitClick()
+{
+	summonerName=document.getElementById('summonerName').innerHTML
+	
+}
+
+function summonerLookUp()
+{
+var summonerName = "";
+var key = "";
+key = $("4d5b95a2-e742-43c9-8d5e-3bec80f09bfe")
+}
