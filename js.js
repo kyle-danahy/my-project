ce6ceef32c3ac17a1546ba58050053da212b256d
magicturtle25 <Pickles124>
2016-06-07 20:00:30 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	HTML.html

--- js.js
+++ js.js
@@ -1,10 +1,8 @@
 var summonerName = "";
 var key = "";
-key = $("4d5b95a2-e742-43c9-8d5e-3bec80f09bfe")
 
 function submitClick()
 {
-	summonerName=document.getElementById('summonerName').innerHTML
 	
 }
 
